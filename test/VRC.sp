54661f421886be526feef4935b31946b152cb85f
Glen Hertz <glen.hertz@gmail.com>
2021-09-01 12:48:38 -0400

    Add working tests

--- test/VRC.sp
+++ test/VRC.sp
@@ -1,6 +1,6 @@
 * The classic RC filter!
-V1 net1 0 DC 0 SIN(0 1 1k)
-R1 net1 net2 1
-C2 net2 0 0.1m
+V1 inp 0 DC 0 SIN(0 1 1k)
+R1 inp out 1
+C2 out 0 0.1m
 .tran 1u 2m
 .END
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,22 @@
 using Xyce
-using Plots
+using Test
 
-res = Xyce.simulate("test/VRC.sp")
-
-plot(res["TIME"], [res["V(NET2)"], res["V(NET1)"]])
-savefig("xyce.png")
+@testset "VRC.sp tran" begin
+	res = Xyce.simulate("VRC.sp")
+	@test length(res) == 3
+	@test length(res["TIME"]) > 100
+	@test res["TIME"][1] == 0.0
+	@test res["TIME"][end] == 0.002
+	@test maximum(res["V(INP)"]) ≈ 1.0 atol=0.01
+	@test minimum(res["V(INP)"]) ≈ -1.0 atol=0.01
+	@test res["V(INP)"][1] == 0
+	@test res["V(INP)"][end] ≈ 0 atol=1e-12
+	@test res["V(OUT)"][1] == 0
+	@test maximum(res["V(OUT)"])/maximum(res["V(INP)"]) ≈ 0.862 atol=0.001
+	i1 = argmax(res["V(INP)"])
+	@test res["V(OUT)"][i1]/res["V(INP)"][i1] ≈ 0.710 atol=0.001
+end
+# This crashes Xyce and Julia (FIXME):
+#@testset "V1D1.sp" begin
+#	res = Xyce.simulate("V1D1.sp")
+#end
